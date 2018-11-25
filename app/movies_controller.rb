b2c32c4d236652bf0df948cd73dd8d3309a3ddaf
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-25 22:18:26 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -16,7 +16,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = m)
-  Movie.create do |m|
+  Movie.create do |args|
     m.title = "Home Alone"
     m.release_date = 1990
   end
